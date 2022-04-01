28313c0766745e90ef179c40540e38b19d8d9c38
Guillaume Larroque <larroque.guillaume@gmail.com>
2022-04-01 11:19:15 +0200

    udpate templates

--- src/Controller/LinkController.php
+++ src/Controller/LinkController.php
@@ -6,13 +6,21 @@ use App\Entity\Link;
 use App\Form\LinkType;
 use App\Repository\LinkRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LinkController extends AbstractController
 {
+    /**
+     * @Route("/{short}", name="link")
+     */
+    public function index(Link $link): Response
+    {
+        return $this->redirect($link->getLink());
+    }
+
     /**
      * @Route("/", name="add")
      */
@@ -22,29 +30,20 @@ class LinkController extends AbstractController
         $form = $this->createForm(LinkType::class, $link);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $link->setShort(uniqid());
-
             $entityManager->persist($link);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Link added');
-            dd($link);
+            $this->addFlash('success', $link);
 
             return $this->redirectToRoute('add');
         }
+
         return $this->render('link/add.html.twig', [
             'controller_name' => 'LinkController',
             'form' => $form->createView(),
-            'links' => count($linkRepository->findAll())
+            'links' => count($linkRepository->findAll()),
         ]);
-    }
-
-    /**
-     * @Route("/{short}", name="link")]
-     */
-    public function index(Link $link): Response
-    {
-        return $this->redirect($link->getLink());
     }
 }
